6db53e1b8719a83114e2de0e2966e7e35139f5c1
jiedaoshu <jiedaoshu@gmail.com>
2019-03-26 05:16:24 +0800

    update demo for mlibrary

---
+++ mLibrary/files/new_page_2/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n],o,_(p,q,r,s,t,u,v,_(),w,_(x,y,z,A,B,_(C,D,E,F),G,null,H,A,I,A,J,K,L,null,M,N,O,P,Q,R,S,N),T,_(),U,_(),V,_(W,[])),X,_(),Y,_());}; 
+var b="url",c="new_page_2.html",d="generationDate",e=new Date(1552408258456.34),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="NewVariable",o="page",p="packageId",q="c971fddebc2c4b64aac84d667da569f3",r="type",s="Axure:Page",t="name",u="New Page 2",v="notes",w="style",x="baseStyle",y="627587b6038d43cca051c114ac41ad32",z="pageAlignment",A="near",B="fill",C="fillType",D="solid",E="color",F=0x4C3399FF,G="image",H="imageHorizontalAlignment",I="imageVerticalAlignment",J="imageRepeat",K="cover",L="favicon",M="sketchFactor",N="0",O="colorStyle",P="appliedColor",Q="fontName",R="Applied Font",S="borderWidth",T="adaptiveStyles",U="interactionMap",V="diagram",W="objects",X="masters",Y="objectPaths";
+return _creator();
+})());
