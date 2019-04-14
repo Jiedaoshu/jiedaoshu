4182576aad6d3772b84931e0ba5b4f03031aa790
jiedaoshu <jiedaoshu@gmail.com>
2019-04-15 01:39:13 +0800

    update mLibrary demo

--- mLibrary/files/照片添加/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[])),W,_(),X,_());}; 
-var b="url",c="照片添加.html",d="generationDate",e=new Date(1552939128647.24),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="e74efa7fb9de478fbbbec6226d78683c",q="type",r="Axure:Page",s="name",t="照片添加",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0x3F3399FF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="cover",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="masters",X="objectPaths";
-return _creator();
-})());
